9038869520b5167b4f9cc9cfe20d47159375cfda
Dunder-P <89883054+Dunder-P@users.noreply.github.com>
2024-01-27 05:51:31 -0500

    Screenshotting
    Maps are now screenshotted and saved in pictures upon generation.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+using System.IO;
 
 public class Main : MonoBehaviour
 {
@@ -25,14 +27,14 @@ public class Main : MonoBehaviour
         clearTiles();
         for (int i = 0; i < GameManager.numTiles; i++)
         {
-            tiles.Add(Instantiate(blockPrefab, new Vector3(200 * Random.Range(1, 9), 200 * Random.Range(1, 5), 0), transform.rotation, theCanvas.transform).GetComponent<MapBlock>());
+            tiles.Add(Instantiate(blockPrefab, new Vector3(200 * UnityEngine.Random.Range(1, 9), 200 * UnityEngine.Random.Range(1, 5), 0), transform.rotation, theCanvas.transform).GetComponent<MapBlock>());
             tiles[i].setBlock(determineTile());
             counter = 0;
             bool foundEmpty = false;
             while (!foundEmpty)
             {
                 counter = 0;
-                tiles[i].transform.position = new Vector3(200 * Random.Range(1, 9), 200 * Random.Range(1, 5), 0);
+                tiles[i].transform.position = new Vector3(200 * UnityEngine.Random.Range(1, 9), 200 * UnityEngine.Random.Range(1, 5), 0);
                 for (int u = 0; u < i; u++)
                 {
                     if (GetWorldSapceRect(tiles[i].blockImage.rectTransform).Overlaps(GetWorldSapceRect(tiles[u].blockImage.rectTransform)))
@@ -62,8 +64,8 @@ public class Main : MonoBehaviour
         {
             int kill = 0;
             GameObject q = Instantiate(blockPrefab, tiles[i].transform.position, transform.rotation, theCanvas.transform);
-            float xmod = 200 * Random.Range(-1, 2);
-            float ymod = 200 * Random.Range(-1, 2);
+            float xmod = 200 * UnityEngine.Random.Range(-1, 2);
+            float ymod = 200 * UnityEngine.Random.Range(-1, 2);
             q.transform.position = q.transform.position + new Vector3(xmod, ymod);
             while (!checkOverlap(q.GetComponent<MapBlock>()))
             {
@@ -76,31 +78,31 @@ public class Main : MonoBehaviour
                     if (kill>1000)
                     {
                         Destroy(q);
-                        int r = Random.Range(0, tiles.Count);
+                        int r = UnityEngine.Random.Range(0, tiles.Count);
                         while(r==i)
                         {
-                            r = Random.Range(0, tiles.Count);
+                            r = UnityEngine.Random.Range(0, tiles.Count);
                         }
                         tiles[i].drawLine(tiles[r]);
                         break;
                     }
-                    xmod = 200 * Random.Range(-1, 2);
-                    ymod = 200 * Random.Range(-1, 2);
+                    xmod = 200 * UnityEngine.Random.Range(-1, 2);
+                    ymod = 200 * UnityEngine.Random.Range(-1, 2);
                     q.transform.position = tiles[i].transform.position + new Vector3(xmod, ymod);
                 }
                 if(checkOverlap(q.GetComponent<MapBlock>())&&checkOverlap(tiles[i]))
                 {
-                    xmod = 200 * Random.Range(-1, 2);
-                    ymod = 200 * Random.Range(-1, 2);
+                    xmod = 200 * UnityEngine.Random.Range(-1, 2);
+                    ymod = 200 * UnityEngine.Random.Range(-1, 2);
                     q.transform.position = tiles[i].transform.position + new Vector3(xmod, ymod);
                 }
             }
             if ((checkOverlap(q.GetComponent<MapBlock>(), true) == tiles[i])||q.transform.position.y<=0)
             {
-                int r = Random.Range(0, tiles.Count);
+                int r = UnityEngine.Random.Range(0, tiles.Count);
                 while (r == i)
                 {
-                    r = Random.Range(0, tiles.Count);
+                    r = UnityEngine.Random.Range(0, tiles.Count);
                 }
                 tiles[i].drawLine(tiles[r]);
             }
@@ -111,10 +113,10 @@ public class Main : MonoBehaviour
             Destroy(q);
 
         }
-        int ra = Random.Range(0, tiles.Count);
+        int ra = UnityEngine.Random.Range(0, tiles.Count);
         while (ra >= tiles.Count - 4)
         {
-            ra = Random.Range(0, tiles.Count);
+            ra = UnityEngine.Random.Range(0, tiles.Count);
         }
         tiles[tiles.Count - 4].drawLine(tiles[tiles.Count - 3], tiles[ra]);
         tiles[tiles.Count - 3].drawLine(tiles[tiles.Count - 2], tiles[ra]);
@@ -122,8 +124,17 @@ public class Main : MonoBehaviour
 
         tiles[tiles.Count - 1].drawLine(tiles[tiles.Count - 4], tiles[ra]);
         #endregion
-        ScreenCapture.CaptureScreenshot(Random.state.ToString() + Random.Range(0,1000000) + ".png");
-        Screen.SetResolution(Screen.resolutions[0].width, Screen.resolutions[0].height, false);
+        string peath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), ("MonOtoTopologicalCortext/" + UnityEngine.Random.seed+UnityEngine.Random.Range(0, 1000000) + ".png"));
+        if(!Directory.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "MonOtoTopologicalCortext/")))
+         {
+            Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "MonOtoTopologicalCortext/"));
+        }
+        ScreenCapture.CaptureScreenshot(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), ("MonOtoTopologicalCortext/" + UnityEngine.Random.seed + UnityEngine.Random.Range(0,1000000) + ".png")));
+        if (!Application.isEditor)
+        {
+            Screen.SetResolution(Screen.resolutions[Screen.resolutions.Length].width, Screen.resolutions[Screen.resolutions.Length].height, false);
+        }
+
     }
     public void clearTiles()
     {
@@ -140,10 +151,10 @@ public class Main : MonoBehaviour
     }
     public void guaranteeTile(int id)
     {
-        int replacement = Random.Range(0, tiles.Count);
+        int replacement = UnityEngine.Random.Range(0, tiles.Count);
         while(countTiles(tiles[replacement].id) <= GameManager.guarNum[tiles[replacement].id])
         {
-            replacement = Random.Range(0, tiles.Count);
+            replacement = UnityEngine.Random.Range(0, tiles.Count);
         }
         tiles[replacement].setBlock(id);
     }
@@ -164,7 +175,7 @@ public class Main : MonoBehaviour
     }
     public int determineTile()
     {
-        int id = Random.Range(-GameManager.nullWeight, GameManager.tileNames.Length);
+        int id = UnityEngine.Random.Range(-GameManager.nullWeight, GameManager.tileNames.Length);
         if(id<1)
         {
             return 0;
